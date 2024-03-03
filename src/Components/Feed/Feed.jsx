9a8944bc01b553277e229c7e6e71f4283471bce7
Ogun Karaoglu <ogunkaraoglu62@gmail.com>
2024-03-03 20:46:30 +0300

    Feed section

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,148 @@
+import React from 'react'
+import './Feed.css'
+import thumbail1 from '../../assets/thumbnail1.png'
+import thumbail2 from '../../assets/thumbnail2.png'
+import thumbail3 from '../../assets/thumbnail3.png'
+import thumbail4 from '../../assets/thumbnail4.png'
+import thumbail5 from '../../assets/thumbnail5.png'
+import thumbail6 from '../../assets/thumbnail6.png'
+import thumbail7 from '../../assets/thumbnail7.png'
+import thumbail8 from '../../assets/thumbnail8.png'
+import { Link } from 'react-router-dom'
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      <Link to={`video/20/4521`} className="card">
+        <img src={thumbail1} alt="thumbail1" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </Link>
+      <div className="card">
+        <img src={thumbail2} alt="thumbail2" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail3} alt="thumbail3" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail4} alt="thumbail4" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail5} alt="thumbail5" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail6} alt="thumbail6" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail7} alt="thumbail7" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail8} alt="thumbail8" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail1} alt="thumbail1" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail2} alt="thumbail2" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail3} alt="thumbail3" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail4} alt="thumbail4" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail5} alt="thumbail5" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail6} alt="thumbail6" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail7} alt="thumbail7" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+      <div className="card">
+        <img src={thumbail8} alt="thumbail8" />
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim
+        </h2>
+        <h3>LoremTitle</h3>
+        <p>15k views &bull; 2 days ago </p>
+      </div>
+    </div>
+  )
+}
+
+export default Feed
