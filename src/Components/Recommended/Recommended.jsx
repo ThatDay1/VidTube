ade8478ee67eadef50d9934214377593756ded50
Ogun Karaoglu <ogunkaraoglu62@gmail.com>
2024-03-04 22:18:57 +0300

    recommended section

---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import './Recommended.css'
+import thumbail1 from '../../assets/thumbnail1.png'
+import thumbail2 from '../../assets/thumbnail2.png'
+import thumbail3 from '../../assets/thumbnail3.png'
+import thumbail4 from '../../assets/thumbnail4.png'
+import thumbail5 from '../../assets/thumbnail5.png'
+import thumbail6 from '../../assets/thumbnail6.png'
+import thumbail7 from '../../assets/thumbnail7.png'
+import thumbail8 from '../../assets/thumbnail8.png'
+
+const Recommended = () => {
+  return (
+    <div className="recommended">
+      <div className="side-video-list">
+        <img src={thumbail1} alt="user 1" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbail2} alt="user 2" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbail3} alt="user 3" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbail4} alt="user 4" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbail5} alt="user 5" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbail6} alt="user 6" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbail7} alt="user 7" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbail8} alt="user 8" />
+        <div className="vid-info">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          </h4>
+          <p>loremTitle</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Recommended
